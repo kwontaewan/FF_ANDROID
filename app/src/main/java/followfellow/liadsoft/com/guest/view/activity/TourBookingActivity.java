cd6193fbb7685b6b0cfbf849b54f86bc7906de29
Kownter <xc200926@gmail.com>
2016-01-21 12:01:56 +0900

    gunter

--- app/src/main/java/followfellow/liadsoft/com/guest/view/activity/TourBookingActivity.java
+++ app/src/main/java/followfellow/liadsoft/com/guest/view/activity/TourBookingActivity.java
@@ -3,28 +3,38 @@ package followfellow.liadsoft.com.guest.view.activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.viewpagerindicator.LinePageIndicator;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import followfellow.liadsoft.com.R;
-import followfellow.liadsoft.com.guest.control.DatePickerControl;
+import followfellow.liadsoft.com.guest.control.CalendarViewListener;
 import followfellow.liadsoft.com.guest.control.adapter.TourBookingViewPagerAdapter;
 import followfellow.liadsoft.com.guest.model.ViewPagerData;
-import me.nlmartian.silkcal.DayPickerView;
 
 /**
  * Created by Gunter on 2016-01-18.
  */
 public class TourBookingActivity extends AppCompatActivity {
-    private DayPickerView calenderView;
     private ViewPager viewPager;
     private TourBookingViewPagerAdapter tourBookingViewPagerAdapter;
     private LinePageIndicator linePageIndicator;
+    private SimpleDateFormat df;
+    private TextView calendarText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour_booking);
-        calenderView = (DayPickerView)findViewById(R.id.calendar_view);
-        calenderView.setController(new DatePickerControl());
+        CompactCalendarView compactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        df = new SimpleDateFormat("yyyy년MM월");
+        calendarText = (TextView)findViewById(R.id.calender_text);
+        compactCalendarView.drawSmallIndicatorForEvents(true);
+        compactCalendarView.setLocale(Locale.KOREAN);
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+        compactCalendarView.setListener(new CalendarViewListener(this,df,calendarText));
         initPager();
     }
     private void initPager(){
@@ -34,4 +44,5 @@ public class TourBookingActivity extends AppCompatActivity {
         viewPager.setAdapter(tourBookingViewPagerAdapter);
         linePageIndicator.setViewPager(viewPager);
     }
+
 }
