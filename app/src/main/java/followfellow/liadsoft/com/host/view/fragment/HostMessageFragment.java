d67bf9e90dddd9c9c1a20a684c21c62f63403cbd
gunter <xc200925@gmail.com>
2016-04-06 20:46:46 +0900

    commit t

---
+++ app/src/main/java/followfellow/liadsoft/com/host/view/fragment/HostMessageFragment.java
@@ -0,0 +1,24 @@
+package followfellow.liadsoft.com.host.view.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import followfellow.liadsoft.com.R;
+
+/**
+ * Created by Gunter on 2016-01-22.
+ */
+public class HostMessageFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(
+                R.layout.h_fragment_message, container, false);
+        return rootView;
+    }
+
+}
